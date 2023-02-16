eb416ece4e1d7738fb1657c3479c0f7147f26712
han3on <70957964+han3on@users.noreply.github.com>
2023-02-16 09:28:57 +0800

    Update botGPT.py

--- botGPT.py
+++ botGPT.py
@@ -20,7 +20,7 @@ user_id = int(env["USER_KEY"])
 @bot.message_handler(func=lambda message: True)
 def get_codex(message):
   if int(message.chat.id) != user_id:
-    bot.send_message("This is an only user bot. Please, to get it for your own go to https://github.com/juan-miii/gpt-telegram-bot")
+    bot.send_message("This boti not for public but private use only.")
   else:
     response = openai.Completion.create(
      #engine = "text-davinci-003",
@@ -38,6 +38,6 @@ def get_codex(message):
       presence_penalty = 0,
       stop = ['"""'])
 
-    bot.send_message(message.chat.id, f'```\n{response["choices"][0]["text"]}\n```', parse_mode="Markdown")
+    bot.send_message(message.chat.id, f'{response["choices"][0]["text"]}', parse_mode="None")
 
 bot.infinity_polling()
